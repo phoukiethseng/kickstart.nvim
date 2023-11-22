5bd813a2ea0eb76ab7f91829f886888d09aeaeef
Phou Kiethseng <puseng123@gmail.com>
2023-11-22 20:48:16 +0800

    Added php to treesitter ensure_installed list

--- init.lua
+++ init.lua
@@ -414,7 +414,7 @@ vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = 
 vim.defer_fn(function()
   require('nvim-treesitter.configs').setup {
     -- Add languages to be installed here that you want installed for treesitter
-    ensure_installed = { 'c', 'cpp', 'go', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash' },
+    ensure_installed = { 'php', 'c', 'cpp', 'go', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash' },
 
     -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
     auto_install = false,
